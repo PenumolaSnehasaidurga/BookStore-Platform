8d27b481df33fba6218c76c1d6f2222967d82cad
PenumolaSnehasaidurga <penumolasnehasaidurga@gmail.com>
2026-08-15 12:28:42 +0530

    book.model.js

--- Backend/model/book.model.js
+++ Backend/model/book.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
-    name: String,
-    price: Number,
-    category: String,
-    image: String,
-    title: String,
+  name: String,
+  price: Number,
+  category: String,
+  image: String,
+  title: String,
+  flipkartLink: String,
 });
+
 const Book = mongoose.model("Book", bookSchema);
 
 export default Book;
